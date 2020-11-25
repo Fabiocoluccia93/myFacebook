906f66d128c4719e65f105e89cc2f42b862efc11
Giuseppe <giuseppe.fahmy@gmail.com>
2020-11-25 11:48:28 +0100

    modifica di giuseppe

--- myFacebook/src/it/corso/isc/Prova.java
+++ myFacebook/src/it/corso/isc/Prova.java
@@ -27,7 +27,7 @@ public class Prova extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.getWriter().append("Modificata da giuseppe: ").append(request.getContextPath());
 	}
 
 	/**
